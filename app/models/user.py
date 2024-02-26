300ad6c44678ee8dff6e0e04de153cf275fabb82
ieromi <mishaieroklis@gmail.com>
2024-02-26 18:40:36 +0300

    first commit

---
+++ app/models/user.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Union
+
+
+class User(BaseModel):
+    id: int
+    first_name: str
+    patronymic: Union[str, None] = None
+    last_name: Union[str, None] = None
+    dob: Union[datetime, None] = None
+    tel_num: str
+    email: Union[str, None] = None
+
+
+external_data = {
+    "id": "123",
+    "first_name": "Анастасия",
+    "tel_num": "+7988888888"
+}
+
+
+user = User(**external_data)
+
+print(user.id)
